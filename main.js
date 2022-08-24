a3798ed6c85156428a7b632f3458570d376a86f5
Largher <fernando.largher@gmail.com>
2022-08-23 23:20:33 -0300

    "agregados objetos para batalla faltan ataques"

--- main.js
+++ main.js
@@ -47,6 +47,33 @@ let pokemonRival
 // SECCION BATALLA POKEMON
 
 let sectionBatalla = document.getElementById("batalla")
+let pokedex = [];
+
+for (let i = 1; i < 26; i++) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${i}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const pokemon = {
+                img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${data.id}.png`,
+
+                id: data.id,
+
+                tipo: data.types[0].type.name,
+
+                nombre: data.name,
+
+                hp: data.stats[0].base_stat,
+
+                ataque: data.stats[1].base_stat,
+
+                defensa: data.stats[2].base_stat,
+
+                especial: data.stats[3].base_stat,
+            };
+            pokedex.push(pokemon);
+        });
+}
+
 
 /*********** DISCURSO ***********/
 
@@ -167,7 +194,6 @@ divPokebola.addEventListener("click", () => {
             subtitulo.innerText = "¡Un rival aparecio y te desafia a una batalla!"
             subtitulo.addEventListener("click", () =>{
                 pokemonElegido = 1
-                console.log(pokemonElegido)
                 seleccionPoke.style.display = "none"
                 sectionBatalla.style.display = "grid"
                 consultarPokemones(pokemonElegido)
@@ -193,7 +219,6 @@ divPokebola2.addEventListener("click", () => {
             subtitulo.innerText = "¡Un rival aparecio y te desafia a una batalla!"
             subtitulo.addEventListener("click", () =>{
                 pokemonElegido = 4
-                console.log(pokemonElegido)
                 seleccionPoke.style.display = "none"
                 sectionBatalla.style.display = "grid"
                 consultarPokemones(pokemonElegido)
@@ -219,7 +244,6 @@ divPokebola3.addEventListener("click", () => {
             subtitulo.innerText = "¡Un rival aparecio y te desafia a una batalla!"
             subtitulo.addEventListener("click", () =>{
                 pokemonElegido = 7
-                console.log(pokemonElegido)
                 seleccionPoke.style.display = "none"
                 sectionBatalla.style.display = "grid"
                 consultarPokemones(pokemonElegido)
@@ -246,7 +270,17 @@ class Pokemon{
 }
 
 
-const bulbasaurObj = new Pokemon("bulbasaur", 01, "planta", 4, 22, "charmander", 22);
+function random(min, max) {
+    return Math.floor((Math.random() * (max - min + 1)) + min);
+}
+
+function comprobarMuerte(hp){
+    if(hp > 0){
+        return false;
+    } else {
+        return true;
+    }
+}
 
 function elegirRival(inicial){
     if(inicial == 1){
@@ -257,6 +291,81 @@ function elegirRival(inicial){
         return 1;
     }
 }
+function obtenerHP(base){
+    return Math.round(10 + (5 / 100 * (base *2 + 15 + 1) + 5)) 
+}
+
+function obtenerOtraStat(base){
+    //return Math.round(((((base + 15) * 2 + ((Math.sqrt(1))/ 4)) * 5)/100) +5) 
+    return Math.round((Math.floor(0.01 * (2 * base + 15 + Math.floor(0.25 * 1)) * 5) + 5))                
+}
+
+function crearObjetoPokemon(pokemon){
+    let poke = new Pokemon(pokemon.name, 
+        pokemon.id, 
+        pokemon.types[0].type.name, 
+        obtenerHP(pokemon.stats[0].base_stat),
+        obtenerOtraStat(pokemon.stats[1].base_stat),
+        obtenerOtraStat(pokemon.stats[2].base_stat),
+        obtenerOtraStat(pokemon.stats[3].base_stat),
+        obtenerOtraStat(pokemon.stats[4].base_stat),
+        5,
+        obtenerHP(pokemon.stats[0].base_stat))
+        return poke
+}
+
+function batallaPokemon(rival, inicial){
+    console.log("BATALLA!")
+    let atacar = document.getElementById("atacar")
+    let hpRival = document.getElementById(`hp-1`)
+    let hpInicial = document.getElementById(`hp-2`)
+    let spriteRival = document.getElementById("spriteRival")
+    let spriteInicial = document.getElementById("spriteInicial")
+    let textoBatalla = document.getElementById("textoBatalla")
+    atacar.addEventListener("click", ()=>{
+        if(random(1,10) <= 9){
+            // colocar algun mensaje diciendo xpokemon ataco y saco tanta vida
+            rival[0].hp -= inicial[0].ataque
+            hpRival.innerText = rival[0].hp
+            console.log(`${rival[0].nombre} a sufrido ${inicial[0].ataque} de daño`)
+            if(comprobarMuerte(rival[0].hp)){
+                hpRival.innerText = 0
+                console.log(`${rival[0].nombre} a sido debilitado`)
+                spriteRival.style.transition = "ease 3s"
+                spriteRival.style.opacity = 0
+                pelea.remove()
+                curar.remove()
+                textoBatalla.style.display = "flex"
+                textoBatalla.style.backgroundColor = "green"
+                mensaje.innerText = `${inicial[0].nombre} vencio a ${rival[0].nombre}`
+                textoBatalla.addEventListener("click", () =>{
+                    sectionBatalla.innerHTML = ""
+                    alert('Felicidades , ganaste!')
+                })
+            }
+        } else {
+            inicial[0].hp -= rival[0].ataque
+            hpInicial.innerText = inicial[0].hp
+            console.log(`${inicial[0].nombre} a sufrido ${rival[0].ataque} de daño`)
+            if(comprobarMuerte(inicial[0].hp)){
+                hpInicial.innerText = 0
+                console.log(`${inicial[0].nombre} a sido debilitado`)
+                spriteInicial.style.transition = "ease 3s"
+                spriteInicial.style.opacity = 0
+                pelea.remove()
+                curar.remove()
+                textoBatalla.style.display = "flex"
+                textoBatalla.style.backgroundColor = "red"
+                mensaje.innerText = `${rival[0].nombre} vencio a ${inicial[0].nombre}`
+                textoBatalla.addEventListener("click", () =>{
+                    sectionBatalla.innerHTML = ""
+                    alert('Lastima , perdiste!')
+                })
+            }
+        }
+    })
+}
+
 
 
 function consultarPokemon(id, num) {
@@ -265,35 +374,48 @@ function consultarPokemon(id, num) {
         response.json()
         .then(function(pokemon){
             crearPokemon(pokemon, num)
+            localStorage.setItem(`pokemon_${id}`, JSON.stringify(pokemon))
         })
     })
 }
 
+function obtenerLocalStorage(pokemonElegido){
+    let poke = JSON.parse(localStorage.getItem(`pokemon_${pokemonElegido}`))
+    return poke;
+}
 
 
 function consultarPokemones(pokemonElegido) {
-    let segundoId = pokemonElegido
-    let primerId = elegirRival(pokemonElegido)
-    consultarPokemon(primerId, 1)
-    consultarPokemon(segundoId, 2)
+    let aliado = pokemonElegido
+    let enemigo = elegirRival(pokemonElegido)
+    consultarPokemon(enemigo, 1)
+    consultarPokemon(aliado, 2)
+    let equipo = [crearObjetoPokemon(obtenerLocalStorage(pokemonElegido))]
+    let equipoRival = [crearObjetoPokemon(obtenerLocalStorage(enemigo))]
+    console.log(equipo[0].ataque)
+    console.log(equipo[0].hp)
+    batallaPokemon(equipoRival, equipo)
 }
 
 function crearPokemon(pokemon, num){
     // convertir data a html
-
     if(num == 1){
         let spriteRival = document.getElementById("spriteRival")
         spriteRival.setAttribute("src", pokemon.sprites.front_default)
+        let nombreRival = document.getElementById(`nombre-${num}`)
+        nombreRival.textContent = pokemon.name
+        let hpRival = document.getElementById(`hp-${num}`)
+        hpRival.textContent = obtenerHP(pokemon.stats[0].base_stat)
+        let hpTotalRival = document.getElementById("hpTotalRival")
+        hpTotalRival.textContent = obtenerHP(pokemon.stats[0].base_stat)
     } else if (num == 2){
         let spriteInicial = document.getElementById("spriteInicial")
         spriteInicial.setAttribute("src", pokemon.sprites.back_default)
+        let nombreInicial = document.getElementById(`nombre-${num}`)
+        nombreInicial.textContent = pokemon.name
+        let hpInicial = document.getElementById(`hp-${num}`)
+        hpInicial.textContent = obtenerHP(pokemon.stats[0].base_stat)
+        let hpTotalInicial = document.getElementById("hpTotalInicial")
+        hpTotalInicial.textContent = obtenerHP(pokemon.stats[0].base_stat)
     }
-
-
-    let nombre = document.getElementById(`nombre-${num}`)
-    nombre.textContent = pokemon.name
-
-    let hp = document.getElementById(`hp-${num}`)
-    hp.textContent = pokemon.stats[0].base_stat
 }
-
